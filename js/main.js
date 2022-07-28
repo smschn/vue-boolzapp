8189c78c89cfbad66243c93989c8d1636d538171
Luca Somaschini <somas99@gmail.com>
2022-07-28 16:52:02 +0200

    Deleted unused comment

--- js/main.js
+++ js/main.js
@@ -240,18 +240,3 @@ var app = new Vue ({
         }
 
 })
-
-/*
-  for (var i = 0; i < cards.length; i++) {
-    // If the text is within the card...
-    if(cards[i].innerText.toLowerCase()
-      // ...and the text matches the search query...
-      .includes(search_query.toLowerCase())) {
-        // ...remove the `.is-hidden` class.
-        cards[i].classList.remove("is-hidden");
-    } else {
-      // Otherwise, add the class.
-      cards[i].classList.add("is-hidden");
-    }
-  }
-*/
